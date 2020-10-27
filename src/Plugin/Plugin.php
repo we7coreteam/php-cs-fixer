8a99229bc601662735cf760207d537a9e0b2cb2e
titrxw <1120309488@qq.com>
2020-10-27 14:49:41 +0800

    修改composer-plugin-api版本，兼容composer2.0

--- src/Plugin/Plugin.php
+++ src/Plugin/Plugin.php
@@ -40,6 +40,12 @@ class Plugin implements PluginInterface, EventSubscriberInterface {
 		$this->io = $io;
 	}
 
+	public function deactivate(Composer $composer, IOInterface $io) {
+	}
+
+	public function uninstall(Composer $composer, IOInterface $io) {
+	}
+
 	/**
 	 * @return array
 	 */
